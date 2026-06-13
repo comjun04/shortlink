c49dfcc1015e4ba48523566585c6dfb18e875453
comjun04 <comjun04@gmail.com>
2026-06-13 14:48:51 +0000

    refactor: clean up template code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,47 +1,30 @@
-import * as fs from 'node:fs'
-import { createFileRoute, useRouter } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/react-start'
-
-const filePath = 'count.txt'
-
-async function readCount() {
-  return parseInt(
-    await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
-  )
-}
-
-const getCount = createServerFn({
-  method: 'GET',
-}).handler(() => {
-  return readCount()
-})
-
-const updateCount = createServerFn({ method: 'POST' })
-  .validator((d: number) => d)
-  .handler(async ({ data }) => {
-    const count = await readCount()
-    await fs.promises.writeFile(filePath, `${count + data}`)
-  })
+import {
+  AppShell,
+  AppShellHeader,
+  AppShellMain,
+  Group,
+  Title,
+} from '@mantine/core'
+import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: Home,
-  loader: async () => await getCount(),
 })
 
 function Home() {
-  const router = useRouter()
-  const state = Route.useLoaderData()
-
   return (
-    <button
-      type="button"
-      onClick={() => {
-        updateCount({ data: 1 }).then(() => {
-          router.invalidate()
-        })
+    <AppShell
+      header={{
+        height: 60,
       }}
+      p="md"
     >
-      Add 1 to {state}?
-    </button>
+      <AppShellHeader>
+        <Group h="100%" px="md">
+          <Title fz="h3">Shortlink</Title>
+        </Group>
+      </AppShellHeader>
+      <AppShellMain>Hello World</AppShellMain>
+    </AppShell>
   )
 }
