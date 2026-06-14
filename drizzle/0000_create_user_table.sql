244ff6b52212dce07efe5502c76d29fd4ddbd62b
comjun04 <comjun04@gmail.com>
2026-06-14 14:38:13 +0000

    feat: add drizzle orm

---
+++ drizzle/0000_create_user_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `user` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`email` text NOT NULL,
+	`email_verified` integer NOT NULL,
+	`image` text,
+	`created_at` integer NOT NULL,
+	`updated_at` integer NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `user_email_unique` ON `user` (`email`);
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,14 @@
-import { betterAuth } from 'better-auth'
+import { betterAuth } from 'better-auth/minimal'
 import { tanstackStartCookies } from 'better-auth/tanstack-start'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { db } from '@/db/instance'
+import { schema } from '@/db/schema'
 
 export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'sqlite',
+    schema,
+  }),
   emailAndPassword: {
     enabled: true,
   },
