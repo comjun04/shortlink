b1e0b60f7598afe12fc542d9394ef45f56057ccd
comjun04 <comjun04@gmail.com>
2026-06-14 05:17:21 +0000

    feat: main page mockup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,10 @@ import {
   AppShell,
   AppShellHeader,
   AppShellMain,
+  Button,
+  Checkbox,
   Group,
+  Table,
   Title,
 } from '@mantine/core'
 import { createFileRoute } from '@tanstack/react-router'
@@ -24,7 +27,56 @@ function Home() {
           <Title fz="h3">Shortlink</Title>
         </Group>
       </AppShellHeader>
-      <AppShellMain>Hello World</AppShellMain>
+      <AppShellMain>
+        <Title order={3} size="h3">
+          All links
+        </Title>
+        <Table>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>
+                <Checkbox />
+              </Table.Th>
+              <Table.Th>Slug</Table.Th>
+              <Table.Th>URL</Table.Th>
+              <Table.Th>Actions</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>
+            <Table.Tr>
+              <Table.Td>
+                <Checkbox />
+              </Table.Td>
+              <Table.Td>sample-id</Table.Td>
+              <Table.Td>
+                <a href="https://google.com">https://google.com</a>
+              </Table.Td>
+              <Table.Td>
+                <Group>
+                  <Button>Edit</Button>
+                  <Button variant="danger">Delete</Button>
+                </Group>
+              </Table.Td>
+            </Table.Tr>
+
+            <Table.Tr>
+              <Table.Td>
+                <Checkbox />
+              </Table.Td>
+              <Table.Td>sample-id</Table.Td>
+              <Table.Td>
+                <a href="https://google.com">https://google.com</a>
+              </Table.Td>
+              <Table.Td>
+                <Group>
+                  <Button>Edit</Button>
+                  <Button variant="danger">Delete</Button>
+                </Group>
+              </Table.Td>
+            </Table.Tr>
+          </Table.Tbody>
+        </Table>
+      </AppShellMain>
     </AppShell>
   )
 }
