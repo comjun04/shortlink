5a1a4551151074a112a0af97e6b526a88a49cb8c
comjun04 <comjun04@gmail.com>
2026-06-16 13:29:12 +0000

    refactor: move appshell and header to layout file

--- src/routes/_main.tsx
+++ src/routes/_main.tsx
@@ -1,6 +1,13 @@
+import { authClient } from '@/lib/auth-client'
 import { getSession } from '@/lib/auth.functions'
-import { Outlet, redirect } from '@tanstack/react-router'
-import { createFileRoute } from '@tanstack/react-router'
+import { AppShell, Group, Menu, UnstyledButton, Text } from '@mantine/core'
+import {
+  Outlet,
+  createFileRoute,
+  redirect,
+  useNavigate,
+} from '@tanstack/react-router'
+import { LuChevronDown, LuLogOut } from 'react-icons/lu'
 
 export const Route = createFileRoute('/_main')({
   component: RouteComponent,
@@ -16,5 +23,48 @@ export const Route = createFileRoute('/_main')({
 })
 
 function RouteComponent() {
-  return <Outlet />
+  const navigate = useNavigate()
+  const { user } = Route.useRouteContext()
+
+  return (
+    <AppShell
+      header={{
+        height: 60,
+      }}
+      p="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md" justify="space-between">
+          <Text fz="h2">Shortlink</Text>
+          <Menu>
+            <Menu.Target>
+              <UnstyledButton>
+                <Group gap="xs">
+                  <Text>{user.name}</Text>
+                  <LuChevronDown size={16} />
+                </Group>
+              </UnstyledButton>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item
+                leftSection={<LuLogOut size={16} />}
+                onClick={() => {
+                  authClient
+                    .signOut()
+                    .then(() => navigate({ to: '/_/login' }))
+                    .catch(console.error)
+                }}
+              >
+                <Text>Logout</Text>
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        </Group>
+      </AppShell.Header>
+
+      <AppShell.Main>
+        <Outlet />
+      </AppShell.Main>
+    </AppShell>
+  )
 }
--- src/routes/_main/index.tsx
+++ src/routes/_main/index.tsx
@@ -1,18 +1,7 @@
-import { authClient } from '@/lib/auth-client'
 import { getSession } from '@/lib/auth.functions'
-import { Menu, Text, UnstyledButton } from '@mantine/core'
-import {
-  AppShell,
-  AppShellHeader,
-  AppShellMain,
-  Button,
-  Checkbox,
-  Group,
-  Table,
-  Title,
-} from '@mantine/core'
-import { createFileRoute, redirect, useNavigate } from '@tanstack/react-router'
-import { LuChevronDown, LuLogOut } from 'react-icons/lu'
+import { Button, Checkbox, Group, Table, Title } from '@mantine/core'
+import { createFileRoute, redirect } from '@tanstack/react-router'
+import { LuPlus } from 'react-icons/lu'
 
 export const Route = createFileRoute('/_main/')({
   beforeLoad: async () => {
@@ -27,94 +16,57 @@ export const Route = createFileRoute('/_main/')({
 })
 
 function Home() {
-  const navigate = useNavigate()
-  const { user } = Route.useRouteContext()
-
   return (
-    <AppShell
-      header={{
-        height: 60,
-      }}
-      p="md"
-    >
-      <AppShellHeader>
-        <Group h="100%" px="md" justify="space-between">
-          <Title fz="h3">Shortlink</Title>
-          <Menu>
-            <Menu.Target>
-              <UnstyledButton>
-                <Group gap="xs">
-                  <Text>{user.name}</Text>
-                  <LuChevronDown size={16} />
-                </Group>
-              </UnstyledButton>
-            </Menu.Target>
-            <Menu.Dropdown>
-              <Menu.Item
-                leftSection={<LuLogOut size={16} />}
-                onClick={() => {
-                  authClient
-                    .signOut()
-                    .then(() => navigate({ to: '/_/login' }))
-                    .catch(console.error)
-                }}
-              >
-                <Text>Logout</Text>
-              </Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
-        </Group>
-      </AppShellHeader>
-      <AppShellMain>
-        <Title order={3} size="h3">
-          All links
-        </Title>
-        <Table>
-          <Table.Thead>
-            <Table.Tr>
-              <Table.Th>
-                <Checkbox />
-              </Table.Th>
-              <Table.Th>Slug</Table.Th>
-              <Table.Th>URL</Table.Th>
-              <Table.Th>Actions</Table.Th>
-            </Table.Tr>
-          </Table.Thead>
-          <Table.Tbody>
-            <Table.Tr>
-              <Table.Td>
-                <Checkbox />
-              </Table.Td>
-              <Table.Td>sample-id</Table.Td>
-              <Table.Td>
-                <a href="https://google.com">https://google.com</a>
-              </Table.Td>
-              <Table.Td>
-                <Group>
-                  <Button>Edit</Button>
-                  <Button variant="danger">Delete</Button>
-                </Group>
-              </Table.Td>
-            </Table.Tr>
+    <>
+      <Group justify="space-between">
+        <Title order={3}>All links</Title>
+        <Button leftSection={<LuPlus size={16} />}>Create</Button>
+      </Group>
+      <Table>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>
+              <Checkbox />
+            </Table.Th>
+            <Table.Th>Slug</Table.Th>
+            <Table.Th>URL</Table.Th>
+            <Table.Th>Actions</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          <Table.Tr>
+            <Table.Td>
+              <Checkbox />
+            </Table.Td>
+            <Table.Td>sample-id</Table.Td>
+            <Table.Td>
+              <a href="https://google.com">https://google.com</a>
+            </Table.Td>
+            <Table.Td>
+              <Group>
+                <Button>Edit</Button>
+                <Button variant="danger">Delete</Button>
+              </Group>
+            </Table.Td>
+          </Table.Tr>
 
-            <Table.Tr>
-              <Table.Td>
-                <Checkbox />
-              </Table.Td>
-              <Table.Td>sample-id</Table.Td>
-              <Table.Td>
-                <a href="https://google.com">https://google.com</a>
-              </Table.Td>
-              <Table.Td>
-                <Group>
-                  <Button>Edit</Button>
-                  <Button variant="danger">Delete</Button>
-                </Group>
-              </Table.Td>
-            </Table.Tr>
-          </Table.Tbody>
-        </Table>
-      </AppShellMain>
-    </AppShell>
+          <Table.Tr>
+            <Table.Td>
+              <Checkbox />
+            </Table.Td>
+            <Table.Td>sample-id</Table.Td>
+            <Table.Td>
+              <a href="https://google.com">https://google.com</a>
+            </Table.Td>
+            <Table.Td>
+              <Group>
+                <Button>Edit</Button>
+                <Button variant="danger">Delete</Button>
+              </Group>
+            </Table.Td>
+          </Table.Tr>
+        </Table.Tbody>
+      </Table>
+    </>
   )
 }
